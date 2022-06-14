0fa56f242c911d317b8986d9e6c4e69448c32c37
Aleksandar Kiryakov <aleksandar.sv.kiryakov@gmail.com>
2022-06-14 17:17:01 +0300

    Lecture 2

---
+++ Lectures/2/guessPassword.java
@@ -0,0 +1,19 @@
+package Lectures;
+
+import java.util.Scanner;
+
+public class guessPassword {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String correctPass="s3cr3t!P@ssw0rd";
+        if (text.equals(correctPass))
+        {
+            System.out.println("Welcome");
+        }
+        else
+        {
+            System.out.println("Wrong password");
+        }
+    }
+}
--- practice/1/ConvertUSDtoBGN.java
+++ practice/1/ConvertUSDtoBGN.java
@@ -1,6 +1,5 @@
 package Practice;
 
-import java.awt.*;
 import java.util.Scanner;
 
 public class ConvertUSDtoBGN {
