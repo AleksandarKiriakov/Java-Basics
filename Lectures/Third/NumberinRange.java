119f178fb3583dab4435fca6e8f97db3b0432609
Aleksandar Kiryakov <aleksandar.sv.kiryakov@gmail.com>
2022-06-26 17:40:44 +0300

    Third lecture and practice

---
+++ Lectures/Third/NumberinRange.java
@@ -0,0 +1,15 @@
+package Lectures.Third;
+
+import java.util.Scanner;
+
+public class NumberinRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        if (number != 0 && number >= -100 && number <= 100) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
